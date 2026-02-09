fd5f143500943dcf32700eb52c7c02660ec85521
m-waleed-saeed <weedimian@gmail.com>
2026-02-09 11:11:19 +0500

    feat: add new components and update dependencies
    - Added new pages: MyBanks, PaymentTransfer, TransactionHistory
    - Introduced AnimatedCounter and DoughnutChart components
    - Created BankCard, Category, Copy, HeaderBox, MobileNav, RightSidebar, SideBar, TotalBalanceBox components
    - Updated PostCSS and Tailwind CSS configurations
    - Enhanced progress component with Radix UI
    - Updated package.json with new dependencies including Chart.js and Lucide React

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 
 const Home = () => {
+  const loggedIn = { firstName: "Muhammad", lastName: "Waleed", email: 'weedimian@gmail.com' }
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className='home-content'>
+        <header className="home-header">
+          <HeaderBox type="greeting" title="Welcome" user={loggedIn?.firstName || "Guest"} subtext="Access and manage your account and transactions efficiently." />
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1350.35} />
+        </header>
+        RECENT TRANSACTION
+      </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{ currentBalance: 500.55 }, { currentBalance: 300 }]} />
+    </section>
   )
 }
 
